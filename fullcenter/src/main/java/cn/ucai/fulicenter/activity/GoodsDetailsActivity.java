f351a029bfea7d631645dbc35f05ffad3e8c690a
xieqiangdiao <765648978@qq.com>
2016-10-18 12:46:23 +0800

    加载单个商品的逻辑处理，

---
+++ fullcenter/src/main/java/cn/ucai/fulicenter/activity/GoodsDetailsActivity.java
@@ -0,0 +1,15 @@
+package cn.ucai.fulicenter.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import uai.cn.fullcenter.R;
+
+public class GoodsDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goods_details);
+    }
+}
