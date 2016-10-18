fd5f0ad59ece5ac7c8f11fca1f41a7645795a68f
xieqiangdiao <765648978@qq.com>
2016-10-18 15:22:50 +0800

    优化跳转到商品详情的方法

--- fullcenter/src/main/java/cn/ucai/fulicenter/utils/MFGT.java
+++ fullcenter/src/main/java/cn/ucai/fulicenter/utils/MFGT.java
@@ -1,24 +1,38 @@
 package cn.ucai.fulicenter.utils;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 
+import cn.ucai.fulicenter.activity.GoodsDetailsActivity;
 import cn.ucai.fulicenter.activity.MainActivity;
 import uai.cn.fullcenter.R;
 
 
 public class MFGT {
-    public static void finish(Activity activity){
+    public static void finish(Activity activity) {
         activity.finish();
-        activity.overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
     }
-    public static void gotoMainActivity(Activity context){
+
+    public static void gotoMainActivity(Activity context) {
         startActivity(context, MainActivity.class);
     }
-    public static void startActivity(Activity context,Class<?> cls){
+
+    public static void startActivity(Activity context, Class<?> cls) {
         Intent intent = new Intent();
-        intent.setClass(context,cls);
+        intent.setClass(context, cls);
+        startActivity(context, intent);
+    }
+
+    public static void gotoGoodsDetailActivity(Context context, int goodId) {
+        Intent intent = new Intent();
+        intent.setClass(context, GoodsDetailsActivity.class);
+        startActivity(context, intent);
+    }
+
+    public static void startActivity(Context context, Intent intent) {
         context.startActivity(intent);
-        context.overridePendingTransition(R.anim.push_left_in,R.anim.push_left_out);
+        ((Activity)context).overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
     }
 }
