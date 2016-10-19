6bdb86baa6796f806578e9c03d7cf9b957928228
xieqiangdiao <765648978@qq.com>
2016-10-19 16:14:28 +0800

    将三个方法提取到BaseActivity,优化代码

---
+++ fullcenter/src/main/java/cn/ucai/fulicenter/activity/BaseActivity.java
@@ -0,0 +1,29 @@
+package cn.ucai.fulicenter.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import cn.ucai.fulicenter.utils.MFGT;
+
+/**
+ * Created by Administrator on 2016/10/19.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    protected abstract void initView();
+
+    protected abstract void initData();
+
+    protected abstract void setListener();
+
+    public void onBackPressed(){
+        MFGT.finish(this);
+    }
+
+}
