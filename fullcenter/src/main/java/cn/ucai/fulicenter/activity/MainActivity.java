bd3533ba39ee821b8930a6b8ec1e0401c25e2e7a
xieqiangdiao <765648978@qq.com>
2016-10-19 13:53:52 +0800

    添加精选，实现精选页面

--- fullcenter/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
+++ fullcenter/src/main/java/cn/ucai/fulicenter/activity/MainActivity.java
@@ -1,16 +1,22 @@
 package cn.ucai.fulicenter.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import cn.ucai.fulicenter.fragment.BoutiqueFragment;
 import cn.ucai.fulicenter.fragment.NewGoodsFragment;
 import cn.ucai.fulicenter.utils.L;
 import uai.cn.fullcenter.R;
@@ -36,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
     View view;
 
     NewGoodsFragment newGoodsFragment;
+    BoutiqueFragment boutiqueFragment;
+
+    int index;
+    int currentIndex=8;
+    Fragment[] mFragment = new Fragment[9];
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +55,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
         L.i("MainActivity onCreate");
+        mFragment[0] = new NewGoodsFragment();
+        mFragment[1] = new BoutiqueFragment();
     }
 
 
@@ -51,20 +65,35 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()) {
 
             case R.id.Boutique:
+                index=1;
                 break;
             case R.id.Category:
                 break;
             case R.id.new_Goods:
-                if(newGoodsFragment==null){
-                    newGoodsFragment=new NewGoodsFragment();
-                    getSupportFragmentManager().beginTransaction().add(R.id.fragment,newGoodsFragment).show(newGoodsFragment).commit();
-                }
+                index=0;
                 break;
             case R.id.Personal:
                 break;
             case R.id.Cars:
                 break;
         }
+        setFragment();
+    }
+
+    private void setFragment() {
+
+        if (index != currentIndex) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            if (currentIndex<5){
+
+                ft.hide(mFragment[currentIndex]);
+            }
+            if (!mFragment[index].isAdded()) {
+                ft.add(R.id.fragment, mFragment[index]);
+            }
+            ft.show(mFragment[index]).commit();
+        }
+        currentIndex = index;
     }
 
     @Override
