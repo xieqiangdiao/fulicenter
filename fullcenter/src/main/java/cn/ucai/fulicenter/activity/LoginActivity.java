7efa06b2d43e058ec8a672baedb00ccd6ca464a9
xieqiangdiao <765648978@qq.com>
2016-10-26 14:05:35 +0800

    修改LoginActivity

--- fullcenter/src/main/java/cn/ucai/fulicenter/activity/LoginActivity.java
+++ fullcenter/src/main/java/cn/ucai/fulicenter/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -89,25 +90,22 @@ public class LoginActivity extends BaseActivity {
     protected void setListener() {
     }
 
-    /* @OnClick({R.id.et_name, R.id.etpassword, R.id.btn_login, R.id.but_register})
-     public void onClick(View view) {
-         switch (view.getId()) {
-             case R.id.et_name:
-                 break;
-             case R.id.etpassword:
-                 break;
-             case R.id.btn_login:
+    @OnClick({R.id.et_name, R.id.etpassword, R.id.btn_login, R.id.but_register})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.et_name:
+                break;
+            case R.id.etpassword:
+                break;
+            case R.id.btn_login:
+                break;
+            case R.id.but_register:
+                MFGT.gotoRegister(this);
+                break;
+        }
 
-             case R.id.but_register:
-                 MFGT.gotoRegister(this);
- //                Intent intent=new Intent(this,RegisterActivity.class);
- //                startActivity(intent);
- //                overridePendingTransition(R.anim.push_right_in,R.anim.push_right_out);
-                 break;
-         }
+    }
 
-     }
- */
     private void checkedInput() {
         if (TextUtils.isEmpty(this.name)) {
             CommonUtils.showShortToast(R.string.user_name_connot_be_empty);
@@ -188,7 +186,7 @@ public class LoginActivity extends BaseActivity {
                 L.e("123" + result);
                 if (result != null) {
                     Result re = ResultUtils.getResultFromJson(result, UserAvatar.class);
-                    if (re.getRetCode()==0&&re.isRetMsg()) {
+                    if (re.getRetCode() == 0 && re.isRetMsg()) {
                         UserAvatar user = (UserAvatar) re.getRetData();
                         FuLiCenterApplication.getInstance().setUserAvatar(user);
                         //ShareprefrenceUtils.
